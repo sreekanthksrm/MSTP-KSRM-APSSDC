319e51b3646f506485ecaa0f2c7af63ed8269941
Srikanth <srekanth502@gmail.com>
2019-09-25 17:47:03 +0530

    sretet

---
+++ PythonPractice/mymath.py
@@ -0,0 +1,18 @@
+def iseven(n1):
+    if n1%2 == 0:
+        return True
+    return False
+
+def isodd(n1):
+    if n1%2 != 0:
+        return True
+    return False
+
+
+def isprime(n1):
+    for i in range(2,n1):
+        if n1%i==0:
+            return False
+    return True    
+    
+        
---
+++ PythonPractice/mymodules/mymath1.py
@@ -0,0 +1,5 @@
+def power(bvalue,expvalue=2):
+    return bvalue**expvalue
+
+def arcir(r):
+    return 3.14*r*r
